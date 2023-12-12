1fd986619a65eeaa0710d34547f3b7c4a7ab571b
Beking0912 <sbqoo@qq.com>
2023-12-11 20:34:30 -0600

    add test files

---
+++ file1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int input = 5;  // Static input
+    if (input > 3) {
+        printf("Input is greater than 3\n");
+    } else {
+        printf("Input is not greater than 3\n");
+    }
+    return 0;
+}
---
+++ file2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+    scanf("%d", &input); // User input
+
+    if (input > 3) {
+        printf("Input is greater than 3\n");
+    } else {
+        printf("Input is not greater than 3\n");
+    }
+    return 0;
+}
